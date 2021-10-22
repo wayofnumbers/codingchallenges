9ab677a6b1a8bf707680967deef360da17b69f35
wayofnumbers <lemuel.li@gmail.com>
2021-10-21 23:35:34 -0500

    Updated HackerRank Python snippets

--- HackerRank_Python.py
+++ HackerRank_Python.py
@@ -11,3 +11,30 @@ reversed(range(10))
 
 #nested list comprehension
 [[i,j,k] for i in range(x) for j in range(y) for k in range(z)]
+
+# set input boilerplate
+N_e = int(input())
+e = set(map(int, input().split()))
+N_f = int(input())
+f = set(map(int, input().split()))
+
+### Set Mutations ###
+_,A=input(),set(map(int,input().split()))
+for i in range(int(input())):
+    op,_=input().split()
+    B=set(map(int,input().split()))
+    eval("A."+op+"(B)")   # use 'eval', less safe, slower than 'getattr'
+print(sum(A))    
+
+if __name__ == '__main__':
+    (_, A) = (int(input()),set(map(int, input().split())))
+    B = int(input())
+    for _ in range(B):
+        (command, newSet) = (input().split()[0],set(map(int, input().split())))
+        getattr(A, command)(newSet)   # use 'getattr'
+print (sum(A))
+
+
+# use all() and any() to do list condition cases
+
+
