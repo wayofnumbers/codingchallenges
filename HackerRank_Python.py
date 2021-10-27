11939486b40dcf97949cfe3d89ff6c5a76c86b8a
wayofnumbers <lemuel.li@gmail.com>
2021-10-27 15:18:36 -0500

    Added Jupyter Notebook for HackerRank Python, also a outline of the 10 tips article

--- HackerRank_Python.py
+++ HackerRank_Python.py
@@ -9,8 +9,9 @@ a = [np.array(input().strip().split(), int) for _ in range(n) ]
 # reversed range  -- Designer Doormat
 reversed(range(10))
 
-#nested list comprehension
+#nested list comprehension, good explanation here:https://spapas.github.io/2016/04/27/python-nested-list-comprehensions/
 [[i,j,k] for i in range(x) for j in range(y) for k in range(z)]
+
 
 # set input boilerplate
 N_e = int(input())
@@ -36,5 +37,23 @@ print (sum(A))
 
 
 # use all() and any() to do list condition cases
+
+# use sort() and sorted() properly, https://realpython.com/python-sort/
+
+# use *value to expand an iterable to its items, often used in function auguments
+
+# collections.Counter()
+#  In Python, dict key can be checked by simply use dict[key], it will return False/0 if the key is not there. 
+numShoes = int(raw_input())
+shoes = collections.Counter(map(int, raw_input().split()))
+numCust = int(raw_input())
+
+income = 0
+
+for i in range(numCust):
+    size, price = map(int, raw_input().split())
+    if shoes[size]:     # this is the best part
+        income += price
+        shoes[size] -= 1
 
 
